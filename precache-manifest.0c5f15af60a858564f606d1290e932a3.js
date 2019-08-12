a28f6fa37b4bfc5ee236b6250ab443a03085936d
josh <joshua830507@gmail.com>
2019-08-13 01:42:32 +0800

    Updates

---
+++ precache-manifest.0c5f15af60a858564f606d1290e932a3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d08f745d2a1b2fc97dc863168a85db",
+    "url": "/react-e-commerce/index.html"
+  },
+  {
+    "revision": "46e55fa4330e6b202451",
+    "url": "/react-e-commerce/static/css/main.5bc5e6c3.chunk.css"
+  },
+  {
+    "revision": "3040bf11184a398bb5fd",
+    "url": "/react-e-commerce/static/js/2.a7abe30d.chunk.js"
+  },
+  {
+    "revision": "46e55fa4330e6b202451",
+    "url": "/react-e-commerce/static/js/main.3375f521.chunk.js"
+  },
+  {
+    "revision": "3967faec140fc7ec1b3d",
+    "url": "/react-e-commerce/static/js/runtime~main.b66956e8.js"
+  }
+]);
