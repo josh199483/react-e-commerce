275bb33b0fe305e4712d0df601547f7094e4d28b
josh <joshua830507@gmail.com>
2019-08-14 01:04:44 +0800

    add header and svg

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Switch, Route } from 'react-router-dom'
 import './App.css';
 import Homepage from './pages/Homepage'
 import Shop from './pages/Shop'
+import Header from './components/Header'
 
 function App() {
   return (
     <div className="App">
+      <Header />
       {/* Switch when find the matching route then stop finding */}
       <Switch>
         {/* exact=true only render exactly the right component */}
