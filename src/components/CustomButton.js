08c6a821f5ce9aa6b3bf3f056783e8ca48e8d381
josh <joshua830507@gmail.com>
2019-09-27 01:54:51 +0800

    update new shop data and addItem action and utils

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,10 +1,10 @@
 import React from 'react'
 import './CustomButton.scss'
 
-const CustomButton = ({ children, isGoogleSignIn, ...restProps }) => {
+const CustomButton = ({ children, isGoogleSignIn, inverted, ...restProps }) => {
   return (
     <button
-      className={`${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
+      className={`${inverted ? 'inverted' : ''} ${isGoogleSignIn ? 'google-sign-in' : ''} custom-button`}
       {...restProps}
     >
       {children}
