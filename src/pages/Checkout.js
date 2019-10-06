4cf7d000d9a9654e2ef7722e704f91d32a728f01
josh <joshua830507@gmail.com>
2019-10-06 17:23:28 +0800

    add stripe integration and update favicon

--- src/pages/Checkout.js
+++ src/pages/Checkout.js
@@ -4,6 +4,8 @@ import { createStructuredSelector } from 'reselect'
 
 import CheckoutItem from '../components/CheckoutItem'
 
+import StripeButton from '../components/StripeButton'
+
 import { selectCartItems, selectCartTotal } from '../redux/cart/cartSelectors'
 
 import './Checkout.scss'
@@ -35,6 +37,12 @@ const CheckoutPage = ({ cartItems, total }) => (
     <div className='total'>
       <span>TOTAL: ${total}</span>
     </div>
+    <div className='test-warning'>
+      Please use the following test credit card for payments
+      <br />
+      4242 4242 4242 4242 - Exp: 01/20 - CVV: 123
+    </div>
+    <StripeButton price={total} />
   </div>
 )
 
